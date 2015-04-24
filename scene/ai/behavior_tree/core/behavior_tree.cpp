d4ed83c0a1e52e094750bfc5d5ddf1fccf2faa87
Nathan Warden <nathanwardenlee@icloud.com>
2015-04-23 21:22:27 -0500

    Simplified the update_behavior_tree function.

--- scene/ai/behavior_tree/core/behavior_tree.cpp
+++ scene/ai/behavior_tree/core/behavior_tree.cpp
@@ -3,14 +3,7 @@
 
 void BehaviorTree::update_behavior_tree() {
 	if ( root_node != NULL ) {
-		Vector<BehaviorTreeNode*> nodes = root_node->get_child_nodes_by_type<BehaviorTreeNode*>();
-		int child_count = nodes.size();
-
-		print_line("Composite with " + String::num_int64(nodes.size()) + " children: " + root_node->get_name());
-
-		for ( int i = 0; i < child_count; i++ ) {
-			nodes[i]->update_child_nodes();
-		}
+		root_node->update_child_nodes();
 	}
 	else {
 		print_line("root_node not assigned!");
